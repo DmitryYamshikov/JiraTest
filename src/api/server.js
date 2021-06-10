5894ed128d48523e4095775aa08d103be2c6a7aa
Dmitry <Yamshikov>
2021-06-10 18:20:32 +0700

    рабочий запрос

--- src/api/server.js
+++ src/api/server.js
@@ -1,12 +1,22 @@
-let baseUrl = 'https://jsonplaceholder.typicode.com/posts';
-
-export default function makeRequest(url='', options={}) {
+const user = {
+   email: "admin@admin.com",
+   password: "admin"
+}
+let baseUrl = `http://65.21.188.112/api/users/login?email=${user.email}&password=${user.password}`;
+export default function makeRequest(url = '', options = {
+   method: 'POST',
+   headers: {
+      'accept': 'application/json',
+      
+   },
+   body: JSON.stringify(user)
+}) {
    fetch(baseUrl + url, options)
-   .then((data)=>data.json())
-   .then((data)=>console.log(data))
+      .then((data) => data.json())
+      .then((data) => console.log(data))
       .catch(error => {
          console.log(`ошибка: ${error}`);
 
       })
-   
 }
+
--- src/components/HeaderSection.vue
+++ src/components/HeaderSection.vue
@@ -1,123 +1,131 @@
 <template>
-  <header class="header">
-    <div class="header__left">
-      <a href="/" class="logo">
-        <img src="../assets/img/logo.png" alt="logo" />
-      </a>
+   <header class="header">
+      <div class="header__left">
+         <a href="/" class="logo">
+            <img src="../assets/img/logo.png" alt="logo" />
+         </a>
 
-      <div class="header-dashboard dropdown">
-        <div>Dashboards</div>
-        <ul class="header-dashboard__drop">
-          <li>
-            <a href="#">View System Dashboard</a>
-          </li>
-        </ul>
+         <div class="header-dashboard dropdown">
+            <div>Dashboards</div>
+            <ul class="header-dashboard__drop">
+               <li>
+                  <a href="#">View System Dashboard</a>
+               </li>
+            </ul>
+         </div>
       </div>
-    </div>
-    <div class="header__right">
-      <div class="form-search">
-        <form>
-          <input type="text" placeholder="Search" />
-        </form>
+      <div class="header__right">
+         <div class="form-search">
+            <form>
+               <input type="text" placeholder="Search" />
+            </form>
+         </div>
+         <div class="header__right-links">
+            <div class="header-link header-link_feedback">feed</div>
+            <div class="header-link header-link_help">
+               <span>?</span>
+            </div>
+            <div class="header-link header-link_login">Log In</div>
+         </div>
       </div>
-      <div class="header__right-links">
-        <div class="header-link header-link_feedback">feed</div>
-        <div class="header-link header-link_help">
-          <span>?</span>
-        </div>
-        <div class="header-link header-link_login">Log In</div>
-      </div>
-    </div>
-  </header>
+   </header>
 </template>
 <script>
-  export default {
-    components: {},
-  };
+   export default {
+      components: {},
+   };
 </script>
-<style lang="scss">
-  .header {
-    min-height: 41px;
-    padding: 5px;
-    background-color: #0747a6;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    &__right,
-    &__left {
+<style scoped lang="scss">
+   .header {
+      min-height: 41px;
+      padding: 0 15px;
+      background-color: #0747a6;
       display: flex;
-      align-items: stretch;
-    }
-
-    &__right-links {
-      display: flex;
-      .header-link {
-        margin: 0 10px;
+      justify-content: space-between;
+      &__right,
+      &__left {
+         display: flex;
+         align-items: stretch;
       }
-    }
-  }
-  .logo {
-    margin-right: 30px;
-    width: 158px;
-    height: 30px;
-    img {
-      width: 100%;
-      height: 100%;
-      object-fit: contain;
-    }
-  }
-  .header-dashboard {
-    color: #fff;
-    cursor: pointer;
-    position: relative;
-    & > div {
-      height: 100%;
-      display: flex;
-      align-items: center;
-      padding: 0 5px;
-      &:hover {
-        background-color: #083376;
-      }
-    }
 
-    &__drop {
-      list-style-type: none;
-      position: absolute;
-      top: 120%;
-      left: 0;
-      width: auto;
-      margin: 0;
-      padding: 10px;
-      background-color: #fff;
-      width: max-content;
-      box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
-    }
-  }
-  .dropdown {
-  }
-  .form-search {
-    margin-right: 20px;
-    width: 170px;
-    input {
-      padding-left: 30px;
-      height: 32px;
-      background-color: #083377;
+      &__right-links {
+         display: flex;
+         .header-link {
+            margin: 0 10px;
+         }
+      }
+   }
+   .logo {
+      margin-right: 30px;
+      width: 158px;
+      height: 30px;
+      img {
+         width: 100%;
+         height: 100%;
+         object-fit: contain;
+      }
+   }
+   .header-dashboard {
       color: #fff;
-      border: 2px solid #0a47a2;
-      font-size: 14px;
-    }
-  }
-  .header-link {
-    color: #fff;
-    cursor: pointer;
+      cursor: pointer;
+      position: relative;
+      & > div {
+         height: 100%;
+         display: flex;
+         align-items: center;
+         padding: 0 5px;
+         &:hover {
+            background-color: #083376;
+         }
+      }
 
-    &_feedback {
-    }
+      &__drop {
+         list-style-type: none;
+         position: absolute;
+         top: 120%;
+         left: 0;
+         width: auto;
+         margin: 0;
+         padding: 10px;
+         background-color: #fff;
+         width: max-content;
+         box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
+         a {
+            color: #000;
+            text-decoration: none;
+            font-size: 14px;
+            &:hover {
+               text-decoration: underline;
+            }
+         }
+      }
+   }
+   .dropdown {
+   }
+   .form-search {
+      margin-right: 20px;
+      width: 170px;
+      input {
+         padding-left: 30px;
+         height: 32px;
+         background-color: #083377;
+         color: #fff;
+         border: 2px solid #0a47a2;
+         font-size: 14px;
+      }
+   }
+   .header-link {
+      color: #fff;
+      cursor: pointer;
 
-    &_help {
-    }
+      &_feedback {
+      }
 
-    &_login {
-    }
-  }
+      &_help {
+      }
+
+      &_login {
+      }
+   }
+   
 </style>
