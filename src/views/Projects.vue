0a46a40414c851d762049e2e64ca2ab171390ecb
Dmitry <DmitryYamshikov@gmail.com>
2021-06-14 20:54:19 +0700

    вывод списка проектов

---
+++ src/views/Projects.vue
@@ -0,0 +1,31 @@
+<template>
+  <h2>Проекты</h2>
+
+  <ul>
+    <li v-for="(item) in projectList" :key="item.id">
+
+        <div class="row">
+          <span>{{ item.id }}</span>
+          <span>{{ item.name }}</span>
+          <span>{{ item.created_at }}</span>
+        </div>
+
+    </li>
+  </ul>
+  <pre>{{this.projectList}}</pre>
+</template>
+<script>
+import {mapGetters, mapActions} from 'vuex'
+
+export default ({
+  computed: {
+    ...mapGetters('projects',['projectList'])
+  },
+  methods:{
+    ...mapActions('projects',['getProjects'])
+  },
+  created() {
+    this.getProjects();
+  }
+})
+</script>
