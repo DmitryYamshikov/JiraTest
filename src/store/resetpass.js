c0b38605fef3741679cb57c09b9db71ee10936d2
dmitryyamshikov <dmitryyamshikov@gmail.com>
2021-06-19 17:15:59 +0700

    привязка input  к store. Написание комментариев

--- src/store/resetpass.js
+++ src/store/resetpass.js
@@ -1,15 +1,22 @@
 export default {
   namespaced: true,
   state: {
-    forgotType: "forgotPassword"
+    forgotType: "forgotPassword", //что необходимо восстановить по умолчанию "Password"
+    inputValue:'' //указываем Имя или Почту
   },
   getters: {
-    forgotType: state => state.forgotType
+    forgotType: state => state.forgotType,
+    inputValue: state =>state.inputValue
   },
   mutations: {
-    setForgotType: (state, value) => (state.forgotType = value)
+    setForgotType: (state, value) => (state.forgotType = value),
+    setInputValue: (state,value)=>(state.inputValue=value)
   },
   actions: {
-    setForgotType: ({ commit }, value) => commit("setForgotType", value)
+    setForgotType: ({ commit }, value) => {
+      commit('setInputValue', '') //при смене radio button сбрпсываем значение Input
+      commit("setForgotType", value)
+    },
+    setInputValue: ({commit}, value) =>commit("setInputValue",value)
   }
 };
--- src/views/ResetPass.vue
+++ src/views/ResetPass.vue
@@ -3,8 +3,8 @@
     <div class="wrapper">
       <h1>Can't access your account?</h1>
       <div class="message">
-        If you can't access Jira, fill in this form and an email will be sent to
-        you with the details to access your account again.
+        If you can't access Jira, fill in this form and an email will be sent to you with the details to access your
+        account again.
       </div>
       <form>
         <fieldset>
@@ -23,22 +23,15 @@
             </div>
             <div class="radio">
               <label
-                ><input
-                  type="radio"
-                  name="forgot"
-                  value="forgotUsername"
-                  v-model="changeForgotType"
-                />Username</label
+                ><input type="radio" name="forgot" value="forgotUsername" v-model="changeForgotType" />Username</label
               >
             </div>
           </div>
         </fieldset>
         <label v-if="changeInputType" class="label-user"
-          ><span>Username</span><i>*</i><input type="text"
+          ><span>Username</span><i>*</i> <input type="text" v-model="setInput"
         /></label>
-        <label v-else class="label-user"
-          ><span>Email</span><i>*</i><input type="text"
-        /></label>
+        <label v-else class="label-user"><span>Email</span><i>*</i> <input type="text" v-model="setInput"/></label>
         <div class="form__footer">
           <button class="btn">Send</button>
           <router-link :to="{ name: 'Login' }">Cancel</router-link>
@@ -48,113 +41,121 @@
   </div>
 </template>
 <script>
-import { mapGetters, mapActions } from "vuex";
+  import { mapGetters, mapActions } from "vuex";
 
-export default {
-  data: () => ({}),
-  computed: {
-    ...mapGetters("resetpass", ["forgotType"]),
-    changeForgotType: {
-      get() {
-        return this.forgotType;
+  export default {
+    data: () => ({}),
+    computed: {
+      ...mapGetters("resetpass", ["forgotType", "inputValue"]),
+      changeForgotType: {
+        get() {
+          return this.forgotType;
+        },
+        set(value) {
+          this.setForgotType(value);
+        },
       },
-      set(value) {
-        this.setForgotType(value);
-      }
+      setInput: {
+        get() {
+          return this.inputValue;
+        },
+        set(value) {
+          this.setInputValue(value);
+        },
+      },
+      changeInputType() {
+        return this.forgotType === "forgotPassword";
+      },
     },
-    changeInputType() {
-      return this.forgotType === "forgotPassword";
-    }
-  },
-  methods: {
-    ...mapActions("resetpass", ["setForgotType"])
-  }
-};
+    methods: {
+      ...mapActions("resetpass", ["setForgotType", "setInputValue"]),
+    },
+  };
 </script>
 <style scoped lang="scss">
-h1 {
-  font-size: 24px;
-  line-height: 1.25;
-  letter-spacing: -0.01em;
-  margin-top: 0;
-  font-weight: 500;
-}
-.message {
-  color: rgb(52, 69, 99);
-  font-size: 14px;
-  padding: 15px 15px 15px 40px;
-  background-color: #ddecff;
-  margin-top: 20px;
-}
-fieldset {
-  display: flex;
-  align-items: center;
-  border: none;
-  margin-top: 10px;
-  label {
+  h1 {
+    font-size: 24px;
+    line-height: 1.25;
+    letter-spacing: -0.01em;
+    margin-top: 0;
+    font-weight: 500;
+  }
+  .message {
+    color: rgb(52, 69, 99);
     font-size: 14px;
-    color: rgb(23, 43, 77);
-    font-weight: 400;
+    padding: 15px 15px 15px 40px;
+    background-color: #ddecff;
+    margin-top: 20px;
   }
-}
-legend {
-  float: left;
-  position: relative;
-  text-align: right;
-  width: 130px;
-  word-wrap: break-word;
-  font-weight: 500;
-  font-size: 14px;
-  color: rgb(107, 119, 140);
-}
-.fieldset__body {
-  display: flex;
-  flex-direction: column;
-  padding-left: 10px;
-}
-.radio {
-  margin: 2px 0;
-}
-.label-user {
-  display: flex;
-  align-items: center;
-  font-weight: 500;
-  font-size: 14px;
-  color: rgb(107, 119, 140);
-  margin-left: 75px;
-  i {
-    color: red;
-  }
-  span {
-    display: inline-block;
-    width: 64px;
-    text-align: right;
-  }
-  input {
-    height: 30px;
-    width: 250px;
-    padding: 3px 4px;
-    border-radius: 3px;
-    border: 2px solid rgb(223, 225, 230);
-    background-color: rgb(250, 251, 252);
-    margin-left: 9px;
-  }
-}
-.form__footer {
-  display: flex;
-  align-items: center;
-  margin-top: 10px;
-  margin-left: 153px;
-  .btn {
-    margin-right: 10px;
-  }
-  a {
-    color: rgb(0, 82, 204);
-    font-size: 14px;
-    text-decoration: none;
-    &:hover {
-      text-decoration: underline;
+  fieldset {
+    display: flex;
+    align-items: center;
+    border: none;
+    margin-top: 10px;
+    label {
+      font-size: 14px;
+      color: rgb(23, 43, 77);
+      font-weight: 400;
     }
   }
-}
+  legend {
+    float: left;
+    position: relative;
+    text-align: right;
+    width: 130px;
+    word-wrap: break-word;
+    font-weight: 500;
+    font-size: 14px;
+    color: rgb(107, 119, 140);
+  }
+  .fieldset__body {
+    display: flex;
+    flex-direction: column;
+    padding-left: 10px;
+  }
+  .radio {
+    margin: 2px 0;
+  }
+  .label-user {
+    display: flex;
+    align-items: center;
+    font-weight: 500;
+    font-size: 14px;
+    color: rgb(107, 119, 140);
+    margin-left: 75px;
+    i {
+      color: red;
+    }
+    span {
+      display: inline-block;
+      width: 64px;
+      text-align: right;
+    }
+    input {
+      height: 30px;
+      width: 250px;
+      padding: 3px 4px;
+      border-radius: 3px;
+      border: 2px solid rgb(223, 225, 230);
+      background-color: rgb(250, 251, 252);
+      margin-left: 9px;
+    }
+  }
+  .form__footer {
+    display: flex;
+    align-items: center;
+    margin-top: 10px;
+    margin-left: 153px;
+    .btn {
+      margin-right: 10px;
+    }
+    a {
+      color: rgb(0, 82, 204);
+      font-size: 14px;
+      text-decoration: none;
+      &:hover {
+        text-decoration: underline;
+      }
+    }
+  }
 </style>
