f1e6dee8eca4648c48e741f62b12272083ab0016
Dmitry <DmitryYamshikov@gmail.com>
2021-06-21 00:15:45 +0700

    вывод пагинации

--- src/store/dash.js
+++ src/store/dash.js
@@ -1,34 +1,33 @@
 import makeRequest from "../api/server";
 
-export default ({
-    namespaced: true,
-    state: {
-        dashList:[]
-    },
-    getters:{
-        dashList: state=>state.dashList
-    },
-    mutations: {
-        getDashboard: (state, obj) =>{
-            state.dashList = obj;
-        }
-    },
-    actions:{
-
-        getDashboard(store) {
-            makeRequest('dashboards', {
-                method: "GET",
-                headers: {
-                    'accept': 'application/json',
-                    'Authorization': `Bearer ${localStorage.getItem('token')}`
-                }
-            })
-            .then(data=>data.json())
-            .then(data=> {
-                store.commit('getDashboard', data.data)
-            })
-        }
+export default {
+  namespaced: true,
+  state: {
+    dashList: []
+  },
+  getters: {
+    userList: state => state.dashList.data,
+    pagination: state => state.dashList.links
+  },
+  mutations: {
+    getDashboard: (state, obj) => {
+      state.dashList = obj;
     }
-
-})
-
+  },
+  actions: {
+    getDashboard(store, page = 0) {
+      makeRequest(`dashboards?page=${page}`, {
+        method: "GET",
+        headers: {
+          accept: "application/json",
+          Authorization: `Bearer ${localStorage.getItem("token")}`
+        }
+      })
+        .then(data => data.json())
+        .then(data => {
+          console.log(data);
+          store.commit("getDashboard", data);
+        });
+    }
+  }
+};
--- src/views/Dash.vue
+++ src/views/Dash.vue
@@ -10,31 +10,42 @@
       </tr>
     </thead>
     <tbody>
-      <tr v-for="item in dashList" :key="item.id">
-        <td>{{item.id}}</td>
+      <tr v-for="item in userList" :key="item.id">
+        <td>{{ item.id }}</td>
         <td>
-          <router-link :to="{name:'DashOne', params:{id:item.id}}">
-            {{item.name}}
+          <router-link :to="{ name: 'DashOne', params: { id: item.id } }">
+            {{ item.name }}
           </router-link>
         </td>
-        <td>{{item.user_id}}</td>
-        <td>{{item.created_at}}</td>
+        <td>{{ item.user_id }}</td>
+        <td>{{ createdDate(item.created_at) }}</td>
       </tr>
     </tbody>
   </table>
+  <div class="pagination">
+    <span v-for="(item, i) in pagination" :key="i" @click="paginationStep(i)">{{
+      item.label
+    }}</span>
+  </div>
 </template>
 <script>
-  import { mapGetters, mapActions } from "vuex";
-
-  export default {
-    computed: {
-      ...mapGetters("dash", ["dashList"]),
+import { mapGetters, mapActions } from "vuex";
+import formDate from "../api/formDate";
+export default {
+  computed: {
+    ...mapGetters("dash", ["userList", "pagination"])
+  },
+  methods: {
+    ...mapActions("dash", ["getDashboard"]),
+    createdDate(string) {
+      return formDate(string);
     },
-    methods: {
-      ...mapActions("dash", ["getDashboard"]),
-    },
-    created() {
-      this.getDashboard();
-    },
-  };
+    paginationStep(index) {
+      this.getDashboard(index);
+    }
+  },
+  created() {
+    this.getDashboard();
+  }
+};
 </script>
