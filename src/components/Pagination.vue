082c3977ecccd16d8c1147020e893c349a6efc2e
Dmitry <DmitryYamshikov@gmail.com>
2021-06-23 00:39:28 +0700

    завершение компонента pagination

--- src/components/Pagination.vue
+++ src/components/Pagination.vue
@@ -1,15 +1,48 @@
 <template>
-  <pre>{{ page }}</pre>
   <div class="pagination">
-    <span v-for="(item, i) in page.links" :key="i">{{ item.label }}</span>
+    <span
+      v-for="(item, i) in page.links"
+      :key="i"
+      @click="$emit('calcnextpage', calcNextPage(i))"
+      :class="item.active && 'active'"
+      v-html="item.label"
+    ></span>
   </div>
 </template>
 
 <script>
 export default {
   props: ["page"],
-  name: "pagination"
+  emits: ["calcnextpage"],
+  name: "pagination",
+  data() {
+    return {};
+  },
+  methods: {
+    calcNextPage(index) {
+      if (index === 0) {
+        return this.page.current_page - 1;
+      } else if (index === this.page.last_page + 1) {
+        return this.page.current_page + 1;
+      } else {
+        return index;
+      }
+    }
+  }
 };
 </script>
 
-<style scoped></style>
+<style scoped lang="scss">
+.pagination {
+  display: flex;
+  justify-content: flex-end;
+  margin-top: 40px;
+  span {
+    padding: 10px;
+    cursor: pointer;
+    &.active {
+      text-decoration: underline;
+    }
+  }
+}
+</style>
